972ddbfaf367a5045ab2b091199bd1e70fda5ff5
Nathan Corbyn <me@nathancorbyn.com>
2025-09-18 11:06:59 +0100

    Force `starship` colours

--- config/starship.nix
+++ config/starship.nix
@@ -27,29 +27,29 @@
         fish_style_pwd_dir_length = 1;
       };
       git_branch = {
-        style = "bold fg:black bg:blue";
+        style = "bold fg:zero bg:blue";
         symbol = "";
         format = "[]($style inverted fg:prev_bg)[$symbol $branch(:$remote_branch) ]($style)";
         truncation_length = 10;
       };
       git_commit = {
         format = "[]($style inverted fg:prev_bg)[ $hash$tag ]($style)";
-        style = "bold fg:black bg:orange";
+        style = "bold fg:zero bg:orange";
         tag_symbol = "  ";
         only_detached = true;
       };
       git_metrics = {
         disabled = true;
         only_nonzero_diffs = false;
-        added_style = "bold fg:black bg:green";
-        deleted_style = "bold fg:black bg:red";
+        added_style = "bold fg:zero bg:green";
+        deleted_style = "bold fg:zero bg:red";
         format = lib.concatStrings [
          "[]($added_style inverted fg:prev_bg)[$added ]($added_style)" 
          "[]($deleted_style inverted fg:prev_bg)[$deleted ]($deleted_style)"
         ];
       };
       git_status = {
-        style = "bold fg:black bg:purple";
+        style = "bold fg:zero bg:purple";
         format = "[]($style inverted fg:prev_bg)[$all_status$ahead_behind ]($style)";
         ahead = "";
         behind = "";
@@ -62,7 +62,7 @@
         up_to_date = "";
       };
       git_state = {
-        style = "bold fg:black bg:yellow";
+        style = "bold fg:zero bg:yellow";
         format = "[]($style inverted fg:prev_bg)[ $state ($progress_current/$progress_total) ]($style)";
       };
       username = {
@@ -74,7 +74,36 @@
       };
       palettes = {
         iro = with config.antidotfiles.colours; {
+          black = "#${black}";
+          red = "#${red}";
+          green = "#${green}";
+          yellow = "#${yellow}";
+          blue = "#${blue}";
+          purple = "#${magenta}";
+          cyan = "#${cyan}";
           orange = "#${orange}";
+
+          brightBlack = "#${brightBlack}";
+          brightRed = "#${brightRed}";
+          brightGreen = "#${brightGreen}";
+          brightYellow = "#${brightYellow}";
+          brightBlue = "#${brightBlue}";
+          brightPurple = "#${brightMagenta}";
+          brightCyan = "#${brightCyan}";
+          brightOrange = "#${brightOrange}";
+
+          zero = "#${zero}";
+          dark0 = "#${dark0}";
+          dark1 = "#${dark1}";
+          dark2 = "#${dark2}";
+          dark3 = "#${dark3}";
+          dark4 = "#${dark4}";
+
+          light0 = "#${light0}";
+          light1 = "#${light1}";
+          light2 = "#${light2}";
+          light3 = "#${light3}";
+          light4 = "#${light4}";
         };
       };
     };
